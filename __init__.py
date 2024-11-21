93acf1fc71c71ec6d0afe7a96e868f0d37bba120
lamps-dev <rimit58872@gmail.com>
2024-11-21 17:01:39 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .maths import *
 from .libs import *
 from .strings import *
-from .management import *
+# from .management import * (Not added yet.)
 from .system import *
 
 __version__ = "0.1.0"
